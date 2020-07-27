890dcb52655feb6160f985fcd3e89dcb66d9d29e
Shay Rojansky <roji@roji.org>
2020-07-27 05:43:56 +0300

    Empty EF Core test template

--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
-namespace Test
+namespace EFGames
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await using var ctx = new BlogContext();
+            await ctx.Database.EnsureDeletedAsync();
+            await ctx.Database.EnsureCreatedAsync();
         }
+    }
+
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        static ILoggerFactory ContextLoggerFactory
+            => LoggerFactory.Create(b => b.AddConsole().AddFilter("", LogLevel.Information));
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                // .UseSqlServer(@"Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0")
+                //.UseSqlite("Filename=:memory:")
+                .UseNpgsql(@"Host=localhost;Username=test;Password=test")
+                .EnableSensitiveDataLogging()
+                .UseLoggerFactory(ContextLoggerFactory);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
