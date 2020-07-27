3b6818f93b13130c7f7d6ca31eb120ee920ad78a
Shay Rojansky <roji@roji.org>
2020-07-27 10:40:10 +0300

    Stage 2
    Take dependency directly on SqlClient.dll, not on project
    Add virtual Save() on DbTransaction

--- SystemData/DbTransaction.cs
+++ SystemData/DbTransaction.cs
@@ -4,5 +4,9 @@ namespace SystemData
 {
     public class DbTransaction
     {
+        public virtual void Save(string savepointName)
+        {
+            Console.WriteLine("DbTransaction implementation called");
+        }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SystemData;
 using SqlClient;
 
 namespace Test
@@ -9,6 +10,9 @@ namespace Test
         {
             var tx = new SqlTransaction();
             tx.Save("foo");
+
+            var dbTransaction = (DbTransaction)tx;
+            dbTransaction.Save("bar");
         }
     }
 }
