dd06f01d5ed58693da041977772bae994e09953c
Shay Rojansky <roji@roji.org>
2020-10-16 09:22:46 +0300

    Top-level statements :)

--- Program.cs
+++ Program.cs
@@ -4,14 +4,5 @@ using Dapper;
 using Npgsql;
 using NpgsqlTypes;
 
-namespace Test
-{
-    public class Program
-    {
-        static async Task Main(string[] args)
-        {
-            await using var connection = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
-            await connection.OpenAsync();
-        }
-    }
-}
+await using var connection = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
+await connection.OpenAsync();
