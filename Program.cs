f0cee7136a2cbcbf2787bee650196ebb4b6f337c
Shay Rojansky <roji@roji.org>
2020-11-12 00:55:56 +0200

    Add SqlConnection

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.Data.SqlClient;
 using Npgsql;
 using NpgsqlTypes;
 
+// await using var connection = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0");
 await using var connection = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
 await connection.OpenAsync();
