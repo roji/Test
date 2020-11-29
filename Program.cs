b9e6b358a9388cda27e5a88a610bc1c34516560a
Shay Rojansky <roji@roji.org>
2020-11-29 16:53:35 +0200

    EF6

--- Program.cs
+++ Program.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Threading.Tasks;
-using Dapper;
-using Microsoft.Data.SqlClient;
 using Npgsql;
 using NpgsqlTypes;
 
-// await using var connection = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0");
-await using var connection = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
-await connection.OpenAsync();
+using var ctx = new BlogContext();
+ctx.Database.Delete();
+ctx.Database.Create();
+
+class BlogContext : DbContext
+{
+    public BlogContext() : base("Host=localhost;Username=test;Password=test")
+    {
+    }
+
+    public DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+    }
+}
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+class NpgsqlConfiguration : DbConfiguration
+{
+    public NpgsqlConfiguration()
+    {
+        SetProviderFactory("Npgsql", NpgsqlFactory.Instance);
+        SetProviderServices("Npgsql", NpgsqlServices.Instance);
+        SetDefaultConnectionFactory(new NpgsqlConnectionFactory());
+    }
+}
