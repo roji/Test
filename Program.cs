cb2c3981dc8d8d9c7bf52d4e9d481f8245797ac9
Shay Rojansky <roji@roji.org>
2021-02-11 12:09:31 +0100

    Fortunes benchmark code

--- Program.cs
+++ Program.cs
@@ -1,36 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Logging;
 
-await using var ctx = new BlogContext();
-await ctx.Database.EnsureDeletedAsync();
-await ctx.Database.EnsureCreatedAsync();
+#pragma warning disable EF1001
 
-public class BlogContext : DbContext
+public class Program
 {
-    public DbSet<Blog> Blogs { get; set; }
+    private const int Iterations = 30000;
 
-    static ILoggerFactory ContextLoggerFactory
-        => LoggerFactory.Create(b => b.AddConsole().AddFilter("", LogLevel.Information));
+    private static DbContextPool<ApplicationDbContext> _contextPool;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder
-            .UseSqlServer(@"Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0")
-            //.UseSqlite("Filename=:memory:")
-            // .UseNpgsql(@"Host=localhost;Username=test;Password=test")
-            .EnableSensitiveDataLogging()
-            .UseLoggerFactory(ContextLoggerFactory);
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public static async Task Main(string[] args)
     {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseNpgsql(@"Host=localhost;Username=test;Password=test",
+            o => o.ExecutionStrategy(d => new NonRetryingExecutionStrategy(d)));
+        var options = optionsBuilder.Options;
+        _contextPool = new DbContextPool<ApplicationDbContext>(options);
+
+        // await using var ctx = new ApplicationDbContext(options);
+        // await ctx.Database.EnsureDeletedAsync();
+        // await ctx.Database.EnsureCreatedAsync();
+
+        Console.WriteLine("About to start, press any key...");
+        Console.ReadKey();
+        Console.WriteLine("Starting run...");
+
+        var sw = Stopwatch.StartNew();
+
+        for (var i = 0; i < Iterations; i++)
+        {
+            await LoadFortunesRows();
+        }
+
+        Console.WriteLine($"Completed {Iterations} iterations in {sw.ElapsedMilliseconds}ms");
+    }
+
+    private static readonly Func<ApplicationDbContext, IAsyncEnumerable<Fortune>> _fortunesQuery
+        = EF.CompileAsyncQuery((ApplicationDbContext context) => context.Fortune);
+
+    public static async Task<IEnumerable<Fortune>> LoadFortunesRows()
+    {
+        var result = new List<Fortune>();
+
+        var poolable = _contextPool.Rent();
+        poolable.SetLease(new DbContextLease(_contextPool, standalone: true));
+
+        using var dbContext = (ApplicationDbContext)poolable;
+
+        try
+        {
+            await foreach (var fortune in _fortunesQuery(dbContext))
+            {
+                result.Add(fortune);
+            }
+        }
+        finally
+        {
+            _contextPool.Return(dbContext);
+        }
+
+        result.Add(new Fortune { Message = "Additional fortune added at request time." });
+
+        result.Sort();
+
+        return result;
     }
 }
 
-public class Blog
+public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
+    public DbSet<Fortune> Fortune { get; set; }
+
+    // static ILoggerFactory ContextLoggerFactory
+    //     => LoggerFactory.Create(b => b.AddConsole().AddFilter("", LogLevel.Information));
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //     => optionsBuilder
+    //         .UseNpgsql(@"Host=localhost;Username=test;Password=test",
+    //             o => o.ExecutionStrategy(d => new NonRetryingExecutionStrategy(d)));
+            // .EnableSensitiveDataLogging()
+            // .UseLoggerFactory(ContextLoggerFactory);
+}
+
+[Table("fortune")]
+public class Fortune : IComparable<Fortune>, IComparable
+{
+    [Column("id")]
     public int Id { get; set; }
-    public string Name { get; set; }
+
+    [NotMapped]
+    [IgnoreDataMember]
+    public int _Id { get; set; }
+
+    [Column("message")]
+    [StringLength(2048)]
+    [IgnoreDataMember]
+    public string Message { get; set; }
+
+    public int CompareTo(object obj)
+    {
+        return CompareTo((Fortune)obj);
+    }
+
+    public int CompareTo(Fortune other)
+    {
+        return String.CompareOrdinal(Message, other.Message);
+    }
 }
