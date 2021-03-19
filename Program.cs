19571ab47ca5607a556def272f809a53517f5342
Shay Rojansky <roji@roji.org>
2021-03-19 11:14:40 +0100

    Rename

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Data.SqlClient;
 using Npgsql;
 using NpgsqlTypes;
 
-// await using var connection = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0");
-await using var connection = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
-await connection.OpenAsync();
+// await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0");
+await using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
+await conn.OpenAsync();
+
