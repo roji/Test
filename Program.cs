4e4028f0b8e7a9c879e73440cb4ca99fc07c9359
Shay Rojansky <roji@roji.org>
2021-09-24 08:43:03 +0300

    Switch to simple logging

--- Program.cs
+++ Program.cs
@@ -14,16 +14,13 @@ public class BlogContext : DbContext
 {
     public DbSet<Blog> Blogs { get; set; }
 
-    static ILoggerFactory ContextLoggerFactory
-        => LoggerFactory.Create(b => b.AddConsole().AddFilter("", LogLevel.Information));
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
             .UseSqlServer(@"Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0")
             //.UseSqlite("Filename=:memory:")
             // .UseNpgsql(@"Host=localhost;Username=test;Password=test")
-            .EnableSensitiveDataLogging()
-            .UseLoggerFactory(ContextLoggerFactory);
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
