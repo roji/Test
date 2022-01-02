3c554521da817d68481c0375e3d20de90a46df22
Shay Rojansky <roji@roji.org>
2022-01-02 13:30:55 +0100

    Template for testing out trimming stuff

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
-using Dapper;
-using Microsoft.Data.SqlClient;
-using Npgsql;
-using NpgsqlTypes;
 
-// await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0");
-await using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
-await conn.OpenAsync();
+Console.WriteLine("Hello world");
 
+_ = GetMembersInHierarchy(typeof(string));
+
+static IEnumerable<MemberInfo> GetMembersInHierarchy(
+    [DynamicallyAccessedMembers(
+        DynamicallyAccessedMemberTypes.PublicProperties
+        | DynamicallyAccessedMemberTypes.NonPublicProperties
+        | DynamicallyAccessedMemberTypes.PublicFields
+        | DynamicallyAccessedMemberTypes.NonPublicFields)]
+    Type type)
+{
+    var currentType = type;
+
+    do
+    {
+        // Do the whole hierarchy for properties first since looking for fields is slower.
+        foreach (var propertyInfo in currentType.GetRuntimeProperties().Where(pi => !(pi.GetMethod ?? pi.SetMethod)!.IsStatic))
+        {
+            yield return propertyInfo;
+        }
+
+        foreach (var fieldInfo in currentType.GetRuntimeFields().Where(f => !f.IsStatic))
+        {
+            yield return fieldInfo;
+        }
+
+        currentType = currentType.BaseType;
+    }
+    while (currentType != null);
+}
