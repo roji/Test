fab9c3ef85cdcb2502c633b5874e5da1983d2167
Shay Rojansky <roji@roji.org>
2022-02-09 16:17:19 +0100

    Add Encrypt=false

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Data.SqlClient;
 using Npgsql;
 using NpgsqlTypes;
 
-// await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0");
-await using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
+await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
 await conn.OpenAsync();
+
 
