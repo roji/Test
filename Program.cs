b47e4f202435389e9c811f358097da8a253eae5c
Shay Rojansky <roji@roji.org>
2022-06-11 19:22:35 +0200

    Test application to test ODBC connection string 1024 limit

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Data.Odbc;
 using System.Threading.Tasks;
+using System.Transactions;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Npgsql;
 using NpgsqlTypes;
 
-// await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0");
-await using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
-await conn.OpenAsync();
+var longString = new string('x', 1025);
+
+// var connString = $"Driver={{ODBC Driver 17 for SQL Server}};server=localhost;database=test;UID=SA;PWD=Abcd5678";
+//var connString = $"Driver={{ODBC Driver 17 for SQL Server}};server={longString};database=test;UID=SA;PWD=Abcd5678";
+
+// var connString = $"Driver={{PostgreSQL Unicode}};server=localhost;database=test;UID=test;PWD=test";
+// var connString = $"Driver={{PostgreSQL Unicode}};server={longString};database=test;UID=test;PWD=test";
+
+// var connString = $"Driver={{SQLite3 ODBC Driver}};server=localhost;database=test";
+//var connString = $"Driver={{SQLite3 ODBC Driver}};server={longString};database={longString}";
+
+// var connString = $"Driver={{MySQL ODBC 8.0 Unicode Driver}};server=localhost;database=test;UID=test;PWD=test";
+var connString = $"Driver={{MySQL ODBC 8.0 Unicode Driver}};server={longString};database=test;UID=test;PWD=test";
+
+var connection = new OdbcConnection(connString);
+connection.Open();
+
+using var command = new OdbcCommand("SELECT 1", connection);
+Console.WriteLine(command.ExecuteScalar());
 
