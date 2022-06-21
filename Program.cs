61fdd9d92caa7f5707a66ad08379e3a3cd5b2e0e
Shay Rojansky <roji@roji.org>
2022-06-22 00:28:30 +0200

    ASP.NET minimal API sample

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Threading.Tasks;
-using Dapper;
-using Microsoft.Data.SqlClient;
-using Npgsql;
-using NpgsqlTypes;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
-// await using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
-await conn.OpenAsync();
+app.MapGet("/", () => "Hello World!");
 
-
+app.Run();
