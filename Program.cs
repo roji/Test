6246f09d6c5147debe99bf877e8c8b2c6d063348
Shay Rojansky <roji@roji.org>
2022-10-06 17:13:37 +0200

    WIP

--- Program.cs
+++ Program.cs
@@ -5,10 +5,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Npgsql;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
 
 await using var ctx = new BlogContext();
 await ctx.Database.EnsureDeletedAsync();
 await ctx.Database.EnsureCreatedAsync();
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddNpgsql()
+    .AddEntityFrameworkCoreInstrumentation() // This causes problem with AddNpgsql
+    .AddConsoleExporter()
+    .Build();
+
+Console.WriteLine($"*** Before: {Activity.Current?.Id}");
+using var context = new BlogContext();
+_ = context.Blogs.Any();
+_ = await context.Blogs.AnyAsync();
+Console.WriteLine($"*** After: {Activity.Current?.Id}");
+
+// Console.WriteLine($"*** Before: {Activity.Current?.Id}");
+// await using var connection = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
+// await connection.OpenAsync();
+// await using (var command = new NpgsqlCommand("SELECT 1", connection))
+// {
+//     // Console.WriteLine("Query: " + await command.ExecuteScalarAsync());
+//     Console.WriteLine("Query: " + command.ExecuteScalar());
+// }
+// Console.WriteLine($"*** After: {Activity.Current?.Id}");
+
+
 
 public class BlogContext : DbContext
 {
@@ -16,11 +43,8 @@ public class BlogContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
-            .UseSqlServer(@"Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false")
-            //.UseSqlite("Filename=:memory:")
-            // .UseNpgsql(@"Host=localhost;Username=test;Password=test")
-            .LogTo(Console.WriteLine, LogLevel.Information)
-            .EnableSensitiveDataLogging();
+            // .UseSqlServer(@"Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
+            .UseNpgsql(@"Host=localhost;Username=test;Password=test");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
