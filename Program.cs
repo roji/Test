f7a2a27110fcd5bb75c13f822b01dace7a3293b5
Shay Rojansky <roji@roji.org>
2022-10-10 12:46:57 +0200

    Code to experiment with SqlClient escalation/non-escalation to distributed transaction

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Dapper;
+using System.Transactions;
 using Microsoft.Data.SqlClient;
-using Npgsql;
-using NpgsqlTypes;
 
-await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
-// await using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
-await conn.OpenAsync();
+using (var scope = new TransactionScope())
+{
+    using var conn1 = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
+    conn1.Open();
+    conn1.Close();
+
+    using var conn2 = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
+    conn2.Open();
+    conn2.Close();
+
+    scope.Complete();
+}
 
 
