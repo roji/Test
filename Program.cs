7be1eb11c9f2ff7bcdd8c8e07c558cbbace7dcf4
Shay Rojansky <roji@roji.org>
2022-11-01 18:45:26 +0200

    Bump to Npgsql 7.0, use NpgsqlDataSource

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Npgsql;
 using NpgsqlTypes;
 
 await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
-// await using var conn = new NpgsqlConnection("Host=localhost;Username=test;Password=test");
 await conn.OpenAsync();
 
-
+// await using var dataSource = NpgsqlDataSource.Create("Host=localhost;Username=test;Password=test");
+// await using var conn = await dataSource.OpenConnectionAsync();
