4915531ecd547f33f48d36c2be22eb0bc12222e6
Shay Rojansky <roji@roji.org>
2022-11-15 19:05:46 +0100

    Repro

--- Program.cs
+++ Program.cs
@@ -1,18 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-await using var ctx = new BlogContext();
+await using var ctx = new MyDbContext();
 await ctx.Database.EnsureDeletedAsync();
 await ctx.Database.EnsureCreatedAsync();
 
-public class BlogContext : DbContext
+await using (MyDbContext dbContext = new())
 {
-    public DbSet<Blog> Blogs { get; set; }
+    await dbContext.Database.EnsureDeletedAsync();
+    await dbContext.Database.EnsureCreatedAsync();
+}
+
+await using (MyDbContext dbContext = new())
+{
+    Book book = new () { Number = 3, Title = "Book3" };
+    dbContext.Add(book);
+
+    await dbContext.SaveChangesAsync();
+}
+
+await using (MyDbContext dbContext = new())
+{
+    BookStore store = new() { Name = "MainBookStore" };
+    dbContext.Add(store);
+
+    dbContext.AddRange(new List<Book>()
+    {
+        new Book() { Number = 1, Title = "Book1" },
+        new Book() { Number = 2, Title = "Book2" },
+        new Book() { Number = 3, Title = "Book3" },
+        new Book() { Number = 4, Title = "Book4" }
+    });
+
+    try
+    {
+        await dbContext.SaveChangesAsync();
+    }
+    catch (DbUpdateException ex) when (ex.InnerException is SqlException sqlException && sqlException.Number == 2601)
+    {
+        Console.WriteLine($"SqlException: {sqlException.Message}");
+        Console.WriteLine($"Duplicates: {string.Join(", ", ex.Entries.Select(e => e.Entity.ToString()).ToArray())}");
+    }
+}
+
+public class MyDbContext : DbContext
+{
+    public DbSet<BookStore> BookStores { get; set; }
+    public DbSet<Book> Books { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
@@ -24,11 +62,31 @@ public class BlogContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<BookStore>();
+        modelBuilder.Entity<Book>(b =>
+        {
+            b.HasIndex(book => book.Number).IsUnique();
+        });
     }
 }
 
-public class Blog
+public sealed class BookStore
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
+
+    public override string ToString()
+        => $"{nameof(BookStore)}: {Name}";
+}
+
+public sealed class Book
+{
+    public int Id { get; set; }
+    public int Number { get; set; }
+    public string? Title { get; set; }
+    public int? BookStoreId { get; set; }
+    public BookStore? BookStore { get; set; }
+
+    public override string ToString()
+        => $"{nameof(Book)}: #{Number} - {Title}";
 }
