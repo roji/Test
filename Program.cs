1bf65125fb8b887d609e6e175b7421d0ed6b5316
Shay Rojansky <roji@roji.org>
2022-11-22 16:14:50 +0100

    Repro

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -10,6 +12,9 @@ await using var ctx = new BlogContext();
 await ctx.Database.EnsureDeletedAsync();
 await ctx.Database.EnsureCreatedAsync();
 
+var p = "foo";
+_ = await ctx.Blogs.Where(x => x.Name1 == p || x.Name2.Contains(p)).FirstOrDefaultAsync();
+
 public class BlogContext : DbContext
 {
     public DbSet<Blog> Blogs { get; set; }
@@ -17,18 +22,15 @@ public class BlogContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
             .UseSqlServer(@"Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false")
-            //.UseSqlite("Filename=:memory:")
-            // .UseNpgsql(@"Host=localhost;Username=test;Password=test")
             .LogTo(Console.WriteLine, LogLevel.Information)
             .EnableSensitiveDataLogging();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-    }
 }
 
 public class Blog
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    [Column(TypeName = "varchar(6)")]
+    public string? Name1 { get; set; }
+    [Column(TypeName = "nvarchar(48)")]
+    public string? Name2 { get; set; }
 }
