a1d4e74c2f413b0544642654c2f9178af0d5ebcd
Shay Rojansky <roji@roji.org>
2022-11-24 13:18:27 +0100

    Minimal sample with Npgsql

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Npgsql;
 using NpgsqlTypes;
 
-await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
-await conn.OpenAsync();
+Console.WriteLine("Hello world");
 
-// await using var dataSource = NpgsqlDataSource.Create("Host=localhost;Username=test;Password=test");
-// await using var conn = await dataSource.OpenConnectionAsync();
