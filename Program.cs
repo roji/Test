359819394237169d1da67d5eca3e71be351dbad8
Shay Rojansky <roji@roji.org>
2022-12-24 14:59:33 +0100

    Blank template for Roslyn games

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Dapper;
-using Microsoft.Data.SqlClient;
-using Npgsql;
-using NpgsqlTypes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Editing;
 
-await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
-await conn.OpenAsync();
-
-// await using var dataSource = NpgsqlDataSource.Create("Host=localhost;Username=test;Password=test");
-// await using var conn = await dataSource.OpenConnectionAsync();
+var workspace = new AdhocWorkspace();
+var syntaxGenerator = SyntaxGenerator.GetGenerator(workspace, LanguageNames.CSharp);
