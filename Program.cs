bf2c6845f7e2974a1af4a098a958f9a7a1e2d021
Shay Rojansky <roji@roji.org>
2023-10-08 22:07:12 +0200

    pgvector minimal sample

--- Program.cs
+++ Program.cs
@@ -1,34 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Pgvector;
+using Pgvector.EntityFrameworkCore;
 
 await using var ctx = new BlogContext();
 await ctx.Database.EnsureDeletedAsync();
 await ctx.Database.EnsureCreatedAsync();
 
+ctx.Items.Add(new Item { Embedding = new Vector(new float[] { 1, 1, 1 }) });
+await ctx.SaveChangesAsync();
+
+var embedding = new Vector(new float[] { 1, 1, 1 });
+var items = await ctx.Items.OrderBy(x => x.Embedding!.L2Distance(embedding)).Take(5).ToListAsync();
+
 public class BlogContext : DbContext
 {
-    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Item> Items { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
-            .UseSqlServer(@"Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false")
-            //.UseSqlite("Filename=:memory:")
-            // .UseNpgsql(@"Host=localhost;Username=test;Password=test")
+            .UseNpgsql("Host=localhost;Username=test;Password=test", o => o.UseVector())
             .LogTo(Console.WriteLine, LogLevel.Information)
             .EnableSensitiveDataLogging();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.HasPostgresExtension("vector");
     }
 }
 
-public class Blog
+public class Item
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+
+    [Column(TypeName = "vector(3)")]
+    public Vector? Embedding { get; set; }
 }
