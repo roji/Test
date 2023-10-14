aba9323501c84446eaf53193859953dcd2082959
Shay Rojansky <roji@roji.org>
2023-10-14 10:00:48 +0200

    Attempted repo

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Npgsql;
+
+public static class Test
+{
+    public static void Run(string connectionString)
+    {
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        var dataSource = dataSourceBuilder.Build();
+        dataSource.Dispose();
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Console.WriteLine("Hello, World!");
+
+var connectionString = "Host=localhost;Port=5432;Database=sk_demo;User ID=postgres;Password=xxxxxxx;";
+Test.Run(connectionString);
