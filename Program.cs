e6ffd94bf6baad74c0352dcb4bd321b2c1a15b6e
Shay Rojansky <roji@roji.org>
2023-11-18 17:36:13 +0100

    Bump versions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,56 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
 BenchmarkRunner.Run<Benchmark>();
 
 public class Benchmark
 {
-    [Benchmark]
-    public void Foo()
+    private static readonly bool BufferBoundsChecks = false;
+
+    public double ReadDoubleWithoutBoundChecks()
     {
+        if (BufferBoundsChecks)
+        {
+        }
+
+        return 8;
+    }
+
+    public double ReadDoubleWithBoundChecks()
+    {
+        CheckBounds(sizeof(double));
+
+        return 8;
+    }
+
+    public double ReadDoubleWithInlineBoundChecks()
+    {
+        if (BufferBoundsChecks)
+        {
+            throw new InvalidOperationException("There is not enough data left in the buffer.");
+        }
+
+        return 8;
+    }
+
+    void CheckBounds(int count)
+    {
+        if (BufferBoundsChecks)
+            Core(count);
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        void Core(int count)
+        {
+            if (count > 8)
+                throw new InvalidOperationException("There is not enough data left in the buffer.");
+        }
+    }
+
+    void CheckBoundsInlined(int count)
+    {
+        if (BufferBoundsChecks && count > 8)
+            throw new InvalidOperationException("There is not enough data left in the buffer.");
     }
 }
