6bc1b501a0485e83734f2f2a64f5daf01eda1ca3
Shay Rojansky <roji@roji.org>
2024-05-26 15:35:19 +0300

    Test playground for Cosmos SDK/LINQ querying

--- Program.cs
+++ Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Azure.Identity;
 using Dapper;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
 using Microsoft.Data.SqlClient;
 using Npgsql;
 using NpgsqlTypes;
 
-await using var conn = new SqlConnection("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false");
-await conn.OpenAsync();
+CosmosClient client = new(
+    "https://192.168.64.6:8081/",
+    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+    new CosmosClientOptions
+    {
+        HttpClientFactory = () => new HttpClient(
+            new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            }),
+        ConnectionMode = ConnectionMode.Gateway
+    });
 
-// await using var dataSource = NpgsqlDataSource.Create("Host=localhost;Username=test;Password=test");
-// await using var conn = await dataSource.OpenConnectionAsync();
+Database database = client.GetDatabase("Northwind");
+Container container = database.GetContainer("NorthwindContext");
+
+// string[] ids = ["Foo", "Bar"];
+// Console.WriteLine(container.GetItemLinqQueryable<Customer>()
+//     .Where(c => new[] { "Foo", "Bar" }.Contains(c.CustomerID))
+//     .ToString());
+
+var customers = container.GetItemLinqQueryable<Customer>(allowSynchronousQueryExecution: true)
+    .Where(c => c.CustomerID == "ALFKI")
+    .ToList();
+
+foreach (var customer in customers)
+{
+    Console.WriteLine(customer.CustomerID);
+}
+
+// using FeedIterator<Customer> setIterator = container.GetItemLinqQueryable<Customer>()
+//     .Where(c => c.CustomerID == "ALFKI")
+//     .ToFeedIterator();
+//
+// var response = await setIterator.ReadNextAsync();
+// Console.WriteLine(response.Count);
+
+public class Customer
+{
+    public string CustomerID { get; set; } = null!;
+    public int[] Ints { get; set; } = null!;
+}
+
