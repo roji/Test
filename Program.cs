6405fa2427cfdf61d4ad01752febf6f42cef9357
Shay Rojansky <roji@roji.org>
2024-09-10 18:52:33 +0200

    Cosmos via EF provider

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -11,33 +13,50 @@ await using var context = new BlogContext();
 await context.Database.EnsureDeletedAsync();
 await context.Database.EnsureCreatedAsync();
 
+context.Blogs.Add(new Blog
+{
+    Id = 1,
+    PartitionKey = "pk1",
+    Name = "foo"
+});
+await context.SaveChangesAsync();
+
+_ = await context.Blogs.ToListAsync();
+
 public class BlogContext : DbContext
 {
     public DbSet<Blog> Blogs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
-            .UseSqlServer(Environment.GetEnvironmentVariable("Test__SqlServer__DefaultConnection"))
-            // .UseNpgsql(Environment.GetEnvironmentVariable("Test__Npgsql__DefaultConnection"))
+            .UseCosmos(
+                "https://localhost:8081",
+                // Windows emulator (Parallels Desktop):
+                // "https://192.168.64.6:8081",
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                "Test",
+                o => o.HttpClientFactory(() => new HttpClient(
+                        new HttpClientHandler
+                        {
+                            ServerCertificateCustomValidationCallback =
+                                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                        }))
+                    .ConnectionMode(ConnectionMode.Gateway))
             .LogTo(Console.WriteLine, LogLevel.Information)
             .EnableSensitiveDataLogging();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Blog>()
+            .ToContainer("test")
+            .HasPartitionKey(b => b.PartitionKey);
     }
 }
 
 public class Blog
 {
     public int Id { get; set; }
+    public string PartitionKey { get; set; }
+
     public string Name { get; set; }
-
-    public List<Post> Posts { get; set; }
-}
-
-public class Post
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public Blog Blog { get; set; }
 }
