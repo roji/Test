bbd3a6c300c4a6978039683789b30b218befc486
Shay Rojansky <roji@roji.org>
2024-09-10 18:52:33 +0200

    Cosmos via EF provider

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -11,25 +13,41 @@ await using var context = new BlogContext();
 await context.Database.EnsureDeletedAsync();
 await context.Database.EnsureCreatedAsync();
 
+_ = await context.Blogs.ToListAsync();
+
 public class BlogContext : DbContext
 {
     public DbSet<Blog> Blogs { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
-            .UseSqlServer("Server=localhost;Database=test;User=SA;Password=Abcd5678;Connect Timeout=60;ConnectRetryCount=0;Encrypt=false")
-            // .UseNpgsql("Host=localhost;Username=test;Password=test")
+            .UseCosmos(
+                "https://192.168.64.6:8081",
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                "Test",
+                o => o.HttpClientFactory(() => new HttpClient(
+                        new HttpClientHandler
+                        {
+                            ServerCertificateCustomValidationCallback =
+                                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                        }))
+                    .ConnectionMode(ConnectionMode.Gateway))
             .LogTo(Console.WriteLine, LogLevel.Information)
             .EnableSensitiveDataLogging();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Blog>()
+            .ToContainer("test")
+            .HasPartitionKey(b => b.PartitionKey);
     }
 }
 
 public class Blog
 {
     public int Id { get; set; }
+    public string PartitionKey { get; set; }
+
     public string Name { get; set; }
 
     public List<Post> Posts { get; set; }
