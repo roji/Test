04d5afe54c5d56c24dfc11ddf9214ff19dc9c057
Shay Rojansky <roji@roji.org>
2024-12-13 08:53:06 +0100

    Update versions

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ public class BlogContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder
             .UseCosmos(
-                "https://192.168.64.6:8081",
+                "https://10.211.55.3:8081",
                 "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
                 "Test",
                 o => o.HttpClientFactory(() => new HttpClient(
